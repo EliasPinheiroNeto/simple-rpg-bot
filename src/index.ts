53f5e82c451f6c5bd978ad561a5f796d9a3130c0
Elias Pinheiro <pinheirinho2004@gmail.com>
2023-10-18 10:45:19 -0300

    button concepts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,48 @@
 import dotenv from 'dotenv'
-import { Client, Events } from 'discord.js'
+import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, Client, Events } from 'discord.js'
 
 dotenv.config()
 const TOKEN = process.env.DISCORD_TOKEN
 
 
-const client = new Client({ intents: ["Guilds", "GuildMessages"] })
+const client = new Client({ intents: ["Guilds", "GuildMessages", "MessageContent"] })
 
 client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`)
 })
 
+client.on("messageCreate", async message => {
+    if (message.content == "oi") {
+        message.reply("Oi meu mano")
+        return;
+    }
+
+    if (message.content == "button") {
+        const confirm = new ButtonBuilder()
+            .setCustomId('confirm')
+            .setLabel("Confirmar")
+            .setStyle(ButtonStyle.Success)
+
+        const row = new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(confirm)
+
+
+        message.channel.send({
+            content: "Tem certeza que quer me dar seu cu?",
+            components: [row]
+        })
+    }
+})
+
 client.on('interactionCreate', async interaction => {
+    if (interaction.isButton() && interaction.customId == "confirm") {
+        // interaction.channel?.send("Apertou o botão, iihh ala")
+        console.log(interaction.message.edit("Ueeeeeeepa"))
+        interaction.deferUpdate()
+        return
+    }
+    console.log(interaction);
+
     if (!interaction.isCommand()) {
         return
     }
