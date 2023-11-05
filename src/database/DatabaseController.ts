387774f4df62c5bcff4c0fd61a3897f4c4aa6352
Elias Pinheiro <pinheirinho2004@gmail.com>
2023-11-05 15:31:10 -0300

    prisma client instance cretion improvement

--- src/database/DatabaseController.ts
+++ src/database/DatabaseController.ts
@@ -1,8 +1,9 @@
 import { PrismaClient } from '@prisma/client'
+import prisma from './prisma'
 
 export default class DatabaseController {
     protected prisma: PrismaClient
     constructor() {
-        this.prisma = new PrismaClient()
+        this.prisma = prisma
     }
 }
